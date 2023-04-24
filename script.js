35488f932788cd289e1a624d1761cbe157abfb6b
utatsu-daiki <127820356+utatsu-daiki@users.noreply.github.com>
2023-04-24 15:57:13 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+const drowButton = document.getElementById("drowButton");
+const fortuneImage = document.getElementById("fortuneImage");
+
+const omikujiImages = [
+  "images/大吉.jpeg",
+  "images/吉.jpeg",
+  "images/中吉.jpeg",
+  "images/小吉.jpeg",
+  "images/末吉.jpeg",
+  "images/凶.jpeg",
+  "images/大凶.jpeg",
+];
+
+function getRondomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+drowButton.addEventListener("click", () => {
+    const randomIndex = getRondomInt(omikujiImages.length);
+    const selectedImage = omikujiImages[randomIndex];
+    fortuneImage.src = selectedImage;
+    fortuneImage.style.display = "block";
+});
